3b78fea96a751e2d53e0371423977a66905d5d35
Cris Kroeze <cris.kroeze@gmail.com>
2023-05-15 19:30:40 -0400

    lab updated

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://0.0.0.0:27017/recipes";
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let recipe = {
+      title: "Lasagna",
+      cuisine: "Italian",
+    };
+    return Recipe.create(recipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    Recipe.find().then((recipesFromDB) => {
+      recipesFromDB.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+    });
+  })
+  .then(() => {
+    const query = { title: "Rigatoni alla Genovese" };
+    return Recipe.findOneAndUpdate(query, { duration: 100 });
+  })
+  .then((updatedRecipe) => {
+    console.log("successfully updated duration ", updatedRecipe);
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("sucessfully deleted Carrot Cake");
+  })
+  .finally(() =>{
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
